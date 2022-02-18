e55cd4406493da012062d70cfba3690569af13f3
daltheb <vtarcanjo@hotmail.com>
2022-02-18 03:30:58 -0300

    Added useState

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,13 @@
+import { Counter } from './components/Counter'
 import { RepositoryList } from './components/RepositoryList'
 
 function App() {
-  return <RepositoryList />
+  return (
+    <>
+      <RepositoryList />
+      <Counter />
+    </>
+  )
 }
 
 export default App
